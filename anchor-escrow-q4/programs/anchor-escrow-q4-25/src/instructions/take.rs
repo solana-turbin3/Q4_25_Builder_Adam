cb837e16a3e018835e120d58e9a55b8313d96902
ABFX15 <adambryant08@hotmail.co.uk>
2025-10-16 15:01:13 +0100

    Update anchor-escrow-q4: refined take instruction and updated dependencies

--- anchor-escrow-q4/programs/anchor-escrow-q4-25/src/instructions/take.rs
+++ anchor-escrow-q4/programs/anchor-escrow-q4-25/src/instructions/take.rs
@@ -54,8 +54,7 @@ pub struct Take<'info> {
         close = maker,
         has_one = maker,
         has_one = mint_a,
-        has_one = mint_b,
-        seeds = [b"escrow", maker.key().as_ref(), escrow.seed.to_le_bytes().as_ref()],
+        seeds = [b"escrow", maker.key().as_ref(), &escrow.seed.to_le_bytes().as_ref()],
         bump = escrow.bump,
     )]
     pub escrow: Account<'info, Escrow>,
